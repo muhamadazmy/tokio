f00e672d7596cb598a54cbb3170b7a6f03bd8591
Muhamad Awad <muhamad@restate.dev>
2025-11-05 18:41:01 +0100

    [ChunksTimeout] Consumes the stream reminder and return the items immediately
    Summary:
    When the underlying stream is an exclusive reference (&mut stream), and we need to drop the `ChunksTimout` stream without losing the buffered items.

---
+++ tokio-stream/tests/stream_chunks_remainder.rs
@@ -0,0 +1,25 @@
+#![warn(rust_2018_idioms)]
+#![cfg(all(feature = "time", not(target_os = "wasi")))] // Wasi does not support panic recovery
+#![cfg(panic = "unwind")]
+
+use std::error::Error;
+use tokio::time::Duration;
+use tokio_stream::{self as stream, StreamExt};
+
+#[tokio::test]
+async fn stream_chunks_remainder() -> Result<(), Box<dyn Error>> {
+    let inner = stream::iter([1, 2, 3, 4]).throttle(Duration::from_millis(2));
+    tokio::pin!(inner);
+
+    let chunked = (&mut inner).chunks_timeout(4, Duration::from_millis(8));
+    tokio::pin!(chunked);
+
+    tokio::select! {
+        Some(_chunk) = chunked.next() => {}
+        _ = tokio::time::sleep(Duration::from_millis(3)) => {}
+    }
+
+    assert_eq!(chunked.into_remainder(), vec![1, 2]);
+    assert_eq!(inner.next().await, Some(3));
+    Ok(())
+}
